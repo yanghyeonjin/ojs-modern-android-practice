0d20ffe92af456ee6ac0c01989ef830280ab8a7e
yanghyeonjin <guswls7081@gmail.com>
2020-12-31 19:20:30 +0900

    * 코루틴 사용해보기

--- app/src/main/java/com/example/ojsmodernandroidpractice/MainActivity.kt
+++ app/src/main/java/com/example/ojsmodernandroidpractice/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.ojsmodernandroidpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.ojsmodernandroidpractice.databinding.ActivityMainBinding
 import com.example.ojsmodernandroidpractice.models.Todo
 import com.example.ojsmodernandroidpractice.utils.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
@@ -19,9 +22,7 @@ class MainActivity : AppCompatActivity() {
         val db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java, "database-name"
-        )
-            .allowMainThreadQueries()
-            .build()
+        ).build()
 
         // 값이 변경되는 것을 감지하여 UI 자동 업데이트를 도와준다!
         db.todoDao().getAll().observe(this, Observer { todos ->
@@ -34,7 +35,10 @@ class MainActivity : AppCompatActivity() {
             val newTodo = Todo(title = mBinding.etTodo.text.toString())
 
             // 데이터베이스에 insert
-            db.todoDao().insert(newTodo)
+            // 데이터베이스 조작은 Main Thread(UI Thread)가 아닌 Worker Thread (Background Thread)에서 작동되어야 한다.
+            lifecycleScope.launch(Dispatchers.IO) {
+                db.todoDao().insert(newTodo)
+            }
         }
     }
 }
